0b321fa3606b13c3d70b3792d305a19f63ddf206
Golokhvastov Dmitry <dargon89@mail.ru>
2020-07-02 17:40:01 +0700

    Создаст и опишет моки для разработки (вопросы по артисту и по жанру)

---
+++ src/mocks/questions.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [
+      {
+        artist: `Пелагея`,
+        picture: `https://api.adorable.io/avatars/128/${Math.random()}`
+      },
+      {
+        artist: `Краснознаменная дивизия имени моей бабушки`,
+        picture: `https://api.adorable.io/avatars/128/${Math.random()}`
+      },
+      {
+        artist: `Lorde`,
+        picture: `https://api.adorable.io/avatars/128/${Math.random()}`
+      }
+    ]
+  },
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+        genre: `blues`
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+        genre: `rock`
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+        genre: `rock`
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+        genre: `jazz`
+      }
+    ]
+  }
+]
