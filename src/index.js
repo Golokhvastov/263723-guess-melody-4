8438fb4c500fb1e3b35736e2c33d3de5d634a714
Golokhvastov Dmitry <dargon89@mail.ru>
2020-07-06 10:31:10 +0700

    Добавит подключение Redux DevTools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import questions from "./mocks/questions.js";
 import App from "./components/app/app.jsx";
 import {reducer} from "./reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
